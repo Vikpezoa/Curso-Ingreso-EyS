3540ed8f6cb920309260b1aa7a02278688011994
Vikpezoa <52410712+Vikpezoa@users.noreply.github.com>
2019-07-12 11:37:09 -0300

    Add files via upload

---
+++ 2-InstruccionIf/jsInstruccionIF (6).js
@@ -0,0 +1,24 @@
+function mostrar()
+{
+//tomo la edad  
+var edad
+edad = document.getElementById("edad").value;
+
+if(edad >= 18)
+{
+    alert("La persona es mayor de edad");
+}
+
+if(edad >= 13 && edad <= 17) 
+
+{
+    alert("La persona es adolescente");
+}
+
+if(edad <13 )
+{
+    alert(" La persona es niño/niña");
+}
+
+
+}//FIN DE LA FUNCIÓN
