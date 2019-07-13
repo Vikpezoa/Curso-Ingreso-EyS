0ee09e22aa2a8581ef09fa6491aba36af5ee01e3
Vikpezoa <52410712+Vikpezoa@users.noreply.github.com>
2019-07-13 01:32:04 -0300

    Add files via upload

--- 2-InstruccionIf/jsInstruccionIF (7).js
+++ 2-InstruccionIf/jsInstruccionIF (7).js
@@ -1,8 +1,22 @@
 function mostrar()
 {
 //tomo la edad  
+var edad;
+var estado;
 
+edad = document.getElementById("edad").value;
+estado = document.getElementById("estadoCivil").value;
+
+
+if(edad<18) 
+{
+
+if((estado) != (Soltero))
+    
+}
+
+{  alert("Es muy pequeño para NO ser soltero");
+}
 	
-
 
 }//FIN DE LA FUNCIÓN
